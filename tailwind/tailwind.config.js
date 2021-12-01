b6a334dedd4aa68d0123a8e955670486a03e0257
ffimnsr <ffimnsr@gmail.com>
2021-12-01 17:35:03 +0800

    updated the tailwind condif and bump version
    Signed-off-by: ffimnsr <ffimnsr@gmail.com>

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -19,7 +19,11 @@ module.exports = {
 
 		},
 	},
-	variants: {},
+	variants: {
+		extend: {
+			margin: ['last'],
+		},
+	},
 	plugins: [
 		// Generate safelist.txt file for use in purgeCSS.
 		require('tailwind-safelist-generator')({
@@ -31,12 +35,17 @@ module.exports = {
 				'px-{padding}',
 				'py-{padding}',
 				'text-{textColor}',
+				'!text-{textColor}',
 				'text-{fontSize}',
 				'font-{fontWeight}',
+				'!font-{fontWeight}',
 				'leading-{lineHeight}',
 				'bg-{backgroundColor}',
 				'hover:bg-{backgroundColor}',
 				'hover:text-{textColor}',
+				'no-underline',
+				'!no-underline',
+				'text-center',
 			],
 		}),
 
